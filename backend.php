84c738e8d575f08a5bd4a2a66c7101ff7d64bc8c
Samuel Brinkmann <samuel.brinkmann@googlemail.com>
2014-03-31 22:31:32 +0200

    Update backend.php

--- backend.php
+++ backend.php
@@ -1,10 +1,5 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: samuel
- * Date: 25.03.14
- * Time: 11:02
- */
+
 
 require('config.php');
 
@@ -14,7 +9,7 @@ class backend extends config{
         try{
             switch($_SERVER['SERVER_NAME']){
                 case 'localhost':
-                    $this->db = new PDO("mysql:host=localhost;dbname=italyProject", "root", "password");
+                    $this->db = new PDO("mysql:host=localhost;dbname=italyProject", "root", "scammo");
                     break;
                 default:
                     $this->db = new PDO("mysql:host=localhost;dbname=$config->dbNameName", "$config->dbNameName", "$config->dbPassword");
@@ -28,9 +23,9 @@ class backend extends config{
     function new_restaurant($newData){
         $STH = $this->db->prepare(
             "INSERT INTO restaurantGuide
-            (restaurantName, description, topListe, ranking, hipsterRanking, website, googlemaps)
+            (restaurantName, description, topListe, ranking, preisLeistung, geschmackRanking,locationRanking, hipsterRanking, website, googlemaps)
             value
-            (:restaurantName, :description, :rangliste,:ranking, :hipsterRanking, :websiteLink, :googlemaps)");
+            (:restaurantName, :description, :rangliste,:ranking, :preisleistungRanking, :geschmackRanking,:locationRanking, :hipsterRanking, :websiteLink, :googlemaps)");
         $STH->execute($newData);
     }
     function get_top_ranking($amount = 3){
@@ -38,20 +33,40 @@ class backend extends config{
         $STH->setFetchMode(PDO::FETCH_OBJ);
         return $STH;
     }
-    function show_ranking($ranking_amount){
+    function get_all_restaurants(){
+        $STH = $this->db->query("SELECT * FROM restaurantGuide ORDER BY restaurantName");
+        $STH->setFetchMode(PDO::FETCH_OBJ);
+        return $STH;
+    }
+    function ranking_set_up($type, $ranking_amount){
         $returnData = "";
         $ranking_amount_full_star = $ranking_amount;
         while($ranking_amount_full_star > 0){
             // $returnData .= "<img src='assets/img/star_full_16.png'/>";
-            $returnData .= "<span class=\"fullstar\"></span>";
+            $returnData .= "<span class='full".$type."'></span>";
             $ranking_amount_full_star--;
         }
         $ranking_amount_empty = 5-$ranking_amount;
         while($ranking_amount_empty > 0){
             // $returnData .= "<img src='assets/img/star_empty_16.png'/>";
-            $returnData .= "<span class=\"emptystar\"></span>";
+            $returnData .= "<span class='empty".$type."'></span>";
             $ranking_amount_empty--;
         }
         return $returnData;
+    }
+    function show_generell_ranking($ranking_amount){
+        return $this->ranking_set_up('star', $ranking_amount);
+    }
+    function show_hipster_ranking($ranking_amount){
+        return $this->ranking_set_up('dreieck', $ranking_amount);
+    }
+    function show_price_ranking($ranking_amount){
+        return $this->ranking_set_up('money', $ranking_amount);
+    }
+    function show_food_ranking($ranking_amount){
+        return $this->ranking_set_up('food', $ranking_amount);
+    }
+    function show_location_ranking($ranking_amount){
+        return $this->ranking_set_up('location', $ranking_amount);
     }
 } 
