62d8925e4864dcb5847f520bc9878d223e2cd901
Samuel Brinkmann <samuel.brinkmann@googlemail.com>
2014-04-01 16:30:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -20,11 +20,11 @@ require('backend.php');
         <section class="topRanking clearfix">
             <?
 //            print_r($backendData->get_top_ranking()->fetchAll()) ;
-                foreach($backendData->get_top_ranking()->fetchAll() as $data){
-                    $restaurantName = $data->restaurantName;
-                    $description = $data->description;
-                    $ranking = $data->ranking;
-                    $hipsterRanking = $data->hipsterRanking;
+                foreach($backendData->get_top_ranking()->fetchAll() as $restaurant){
+                    $restaurantName = $restaurant->restaurantName;
+                    $description = $restaurant->description;
+                    $ranking = $restaurant->ranking;
+                    $hipsterRanking = $restaurant->hipsterRanking;
             ?>
                 <article class="clearfix">
                     <h3 class="clearfix"><?=$restaurantName?></h3>
@@ -58,17 +58,49 @@ require('backend.php');
         </section>
         <section class="googleMaps" id="map"></section>
         <section class="allRestaurants">
-            #empty 
+            <?
+            //            print_r($backendData->get_top_ranking()->fetchAll()) ;
+            foreach($backendData->get_all_restaurants()->fetchAll() as $restaurant){
+                $restaurantName = $restaurant->restaurantName;
+                $description = $restaurant->description;
+                $ranking = $restaurant->ranking;
+                $hipsterRanking = $restaurant->hipsterRanking;
+                $website = $restaurant->website;
+
+                ?>
+                <article class="clearfix">
+                    <h3 class="clearfix"><?=$restaurantName?></h3>
+                    <ul>
+                        <li>Ranking: <?=$backendData->show_ranking($ranking)?> <!--(<?=$ranking?>/5)--></li>
+                        <li>Hipster Ranking: <?=$backendData->show_ranking($hipsterRanking)?></li>
+                    </ul>
+                     <p class="clearfix">
+                        <?=$description?>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+                </article>
+            <?
+            }
+            ?>
         </section>
+
         <footer>
             <ul class="clearfix">
                 <li><a href="mailto:kontakt@mif-sh.de?subject=SchülerRestaurantGuide Kontakt" title="Schreib uns eine E-Mail" target="_self">kontakt@mif-sh.de</a></li>
                 <li><a href="#impressum.php?noch-in-arbeit" title="Impressum" target="_self">Impressum</a></li>
             </ul>
         </footer>
-        <script src="https://maps.googleapis.com/maps/api/js?key=&sensor=false&extension=.js"></script> 
+<!--        <script src="https://maps.googleapis.com/maps/api/js?key=&sensor=false&extension=.js"></script> -->
         <script>
-            <? echo file_get_contents('assets/js/main.min.js'); ?>
+            <?
+            echo file_get_contents('https://maps.googleapis.com/maps/api/js?key=&sensor=false&extension=.js');
+            echo file_get_contents('assets/js/main.min.js');
+            ?>
         </script>
     </body>
 </html>
